e67981065c89b30a5a3499c2be306471d22bb3b1
Parth Makwana <makwanaparth1820@gmail.com>
2024-12-10 11:35:21 +0530

    Sample Project For React

---
+++ src/components/ComponentFour.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function componentFour() {
+  return (
+    <div>componentFour</div>
+  )
+}
+
+export default componentFour
---
+++ src/components/userContext.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function userContext() {
+  return (
+    <div>Context API</div>
+  )
+}
+
+export default userContext  
